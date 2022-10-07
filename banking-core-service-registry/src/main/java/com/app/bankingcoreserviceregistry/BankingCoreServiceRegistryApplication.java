1ab16ce6cc7780f73e57fced68b7f43f5e2bd0d6
p.kruk2 <p.kruk2@partner.samsung.com>
2022-10-07 18:21:58 +0200

    Initial commit, service registry app

---
+++ banking-core-service-registry/src/main/java/com/app/bankingcoreserviceregistry/BankingCoreServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.app.bankingcoreserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingCoreServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingCoreServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ banking-core-service-registry/src/test/java/com/app/bankingcoreserviceregistry/BankingCoreServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bankingcoreserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCoreServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
