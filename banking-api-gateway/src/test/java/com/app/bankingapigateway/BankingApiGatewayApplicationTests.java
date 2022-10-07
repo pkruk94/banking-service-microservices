1ec2531c648033e7bd87bda2403285f24452c279
p.kruk2 <p.kruk2@partner.samsung.com>
2022-10-07 18:41:47 +0200

    Initial commit for API Gateway

---
+++ banking-api-gateway/src/test/java/com/app/bankingapigateway/BankingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bankingapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
